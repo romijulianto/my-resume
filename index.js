5147e7b7867a1893fab55d00841a92a4b1e5f539
romijulianto <romyjulians@gmail.com>
2021-04-17 14:32:45 +0700

    get port number via env var

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ fastify.get('/', async (request, reply) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(5000)
+    await fastify.listen(process.env.PORT)
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
